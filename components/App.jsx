845a4fa994fdca3f8ea8af56733706c273ea7435
GrafDrakula-BlaBlaBla <bogdanlevchenko11.l@gmail.com>
2020-12-19 22:07:10 +0300

    add folder components

---
+++ components/App.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable array-callback-return */
+import React, { useEffect } from "react";
+
+import Graph from "./Chart/Chart";
+import DataCovid19 from "../modules/data/data.js";
+
+// const Li = ({ Country }) => {
+//   return <li>{Country}</li>;
+// };
+
+function App() {
+  const dataCovid19 = new DataCovid19();
+
+  const countryTotal = useEffect(() => {
+    const countryTotal = dataCovid19.getCountryTotal().then((result) => {
+      return result;
+    });
+  });
+  const list = countryTotal.map((el) => <li>${el}</li>);
+
+  return (
+    <div className="wrapper">
+      <p>{`Graph`}</p>
+      <ul></ul>
+      <Graph />
+    </div>
+  );
+}
+export default App;
