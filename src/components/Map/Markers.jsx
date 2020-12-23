e442f95f62ebeb953660fa27bd097435925c6a24
Aleksandr <foxi4554@gmail.com>
2020-12-23 15:32:51 +0300

    feat: synchronized data

--- src/components/Map/Markers.jsx
+++ src/components/Map/Markers.jsx
@@ -1,42 +1,65 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Tooltip, Marker } from "react-leaflet";
 import L  from 'leaflet';
-import icon from '../../modules/assets/circle.svg';
 import dataJSON from '../../modules/data/country.js';
 import MarkerStatistic from './MarkerStatistic/MarkerStatistic';
 
-  // function changeIcon(size) {
-  //   const newSize  = isNaN(size) ? 0 : size + 10;
-  //   onChangeIcon(
-  //     L.divIcon({
-  //     shadowUrl: icon,
-  //     shadowSize: [12, 12],
-  //     iconSize: [newSize, newSize],
-  //     popupAnchor: [-10, -10],
-  //   }))
-  // }
-
-const myIconStart = L.divIcon({
-  shadowUrl: icon,
-  shadowSize: [12, 12],
-  iconSize: [10, 10],
-  popupAnchor: [-10, -10],
-})
-
 export default function Markers({ statisticsData, population, selectedCategory }){
-  const [myIcon] = useState(myIconStart);
+  let myIcon = L.divIcon();
   
   const markers = dataJSON.map(( item ) => {
     const currentCountry = statisticsData.Countries.find((country) => {
       return country.CountryCode === item.alpha2;
     });
+
     const position = [item.latitude, item.longitude];
-    const currentPopulation = !currentCountry ? null
-      :population.find((item) => {
+
+    let currentPopulation = null;
+    let iconSize = 10;
+
+    if(currentCountry) {
+      currentPopulation = population.find((item) => {
         return item.alpha2Code === currentCountry.CountryCode;
       });
+
+      const numberPeople = 100000;
+
+      if(selectedCategory === 'total') {
+        iconSize = Math.round((currentCountry.TotalConfirmed / 500000) * 4);
+      } else if(selectedCategory === 'oneDay') {
+        iconSize = Math.round((currentCountry.NewConfirmed / 5000) * 4);
+      }else if(selectedCategory === 'total100') {
+        iconSize = Math.round(((currentCountry.TotalConfirmed  / currentPopulation.population) * numberPeople) / 100);
+      }else if(selectedCategory === 'oneDay100') {
+        iconSize = Math.round(((currentCountry.NewConfirmed / currentPopulation.population) * numberPeople));
+      }
+
+      if(iconSize < 10) {
+        iconSize = 10;
+      } else if(iconSize > 160) {
+        iconSize = 160;
+      }
+    }
+
+    const iconStiles = `
+      width: 100%;
+      height: 100%;
+      background-color: rgba(255, ${160 - iconSize}, ${160 - iconSize}, .85);
+      border-radius: 50%;
+    `
+
+    myIcon = L.divIcon({
+      iconSize: [iconSize < 10 ? 10 : iconSize, iconSize < 10 ? 10 : iconSize],
+      popupAnchor: [-10, -10],
+      html: `<div style="${iconStiles}"></div>`
+    });
     return (
-      <Marker position = { position }  icon={ myIcon } key= { item.numeric } kind={ 'total' } >
+      <Marker
+        position={ position }
+        icon={ myIcon }
+        key={ item.numeric }
+        kind={ 'total' }
+        >
         <Tooltip>
           <div className="flag-for-popUp">
             <img src={ `https://www.countryflags.io/${item.alpha2}/flat/32.png` } alt="flag"/>  { item.country }
